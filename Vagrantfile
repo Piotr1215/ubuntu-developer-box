f06e18a4d5b162232148c0501aeee652d004294a
decoder <decoder@lilve.de>
2020-11-30 17:15:34 +0100

    install.sh

--- Vagrantfile
+++ Vagrantfile
@@ -12,8 +12,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Oh My ZSH Install section
 
   # Install git and zsh prerequisites
-  config.vm.provision :shell, path: "https://gist.github.com/Piotr1215/fe37f0437b5661ec3b68e1685b6ed47a#file-vagrant-provision-sh"
-
+  config.vm.provision :shell, path: => install.sh
 
   ############################################################
 
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo 'Bootstrap steps start here:'
+
+echo '[STEP 1] Installing base packages'
+
+echo '[STEP 2] Updating apt-get repositories...'
+apt-get update
+
+echo '[STEP 3] Installing base packages'
+
+apt-get -y install git
+apt-get -y install zsh
+apt-get -y install figlet
+apt-get -y install tmux
+apt-get -y install wget
+apt-get -y install htop
+
+echo '[STEP 4] Installing Oh-My-Zsh'
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo '[STEP 5] Installing zsh-autosuggestions plugin'
+git clone https://github.com/zsh-users/zsh-autosuggestions /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+echo '[STEP 6] Install tmux with cool customizations'
+git clone https://github.com/samoshkin/tmux-config.git
+./tmux-config/install.sh
+
+echo '[STEP 7] Clone my dotfiles'
+git clone git://github.com/Piotr1215/dotfiles.git ~/.dotfiles
+
+echo '[STEP 8] Copy dotfiles over exisitng ones'
+cp ~/.dotfiles/.zshrc ~/.zshrc
+
+echo '[STEP 9] Setting zsh as default shell'
+chsh -s $(which zsh)

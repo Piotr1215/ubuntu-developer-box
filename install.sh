9e64853edd5d603784a763cec58b3b8138366daf
decoder <decoder@lilve.de>
2020-11-30 19:05:03 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
-echo 'Bootstrap steps start here:'
 
-echo '[STEP 1] Installing base packages'
+echo '[START] Starting Provisioning Process'
 
-echo '[STEP 2] Updating apt-get repositories...'
-apt-get update
+echo '[INSTALLATION] Installing base packages'
 
-echo '[STEP 3] Installing base packages'
+apt-get update && apt-get -y install --no-install-recommends \
+    git \
+    zsh \
+    curl \
+    figlet \
+    tmux \
+    wget \
+    htop
 
-apt-get -y install git
-apt-get -y install zsh
-apt-get -y install figlet
-apt-get -y install tmux
-apt-get -y install wget
-apt-get -y install htop
+echo '[CLONING] Installing Oh-My-Zsh'
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-echo '[STEP 4] Installing Oh-My-Zsh'
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+echo '[CLONING] Installing zsh-autosuggestions plugin'
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-echo '[STEP 5] Installing zsh-autosuggestions plugin'
-git clone https://github.com/zsh-users/zsh-autosuggestions /home/vagrant/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+echo '[INSTALLATION] Install tmux with cool customizations'
+git clone https://github.com/samoshkin/tmux-config.git ~/tmux-config
+~/tmux-config/install.sh
 
-echo '[STEP 6] Install tmux with cool customizations'
-git clone https://github.com/samoshkin/tmux-config.git /home/vagrant/tmux-config
-/home/vagrant/tmux-config/install.sh
+echo '[CLONING] Clone my dotfiles'
+git clone git://github.com/Piotr1215/dotfiles.git ~/.dotfiles
 
-echo '[STEP 7] Clone my dotfiles'
-git clone git://github.com/Piotr1215/dotfiles.git /home/vagrant/.dotfiles
+echo '[INSTALLATION] Installing kubectl'
+(
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - && \
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list && \
+    apt-get update && apt-get -y install --no-install-recommends kubectl
+)
 
-echo '[STEP 8] Copy dotfiles over exisitng ones'
-cp ~/.dotfiles/.zshrc /home/vagrant/.zshrc
+echo '[INSTALLATION] Installing kubectx and kubens - quickly switch kubernetes context and namespace'
+(
+  git clone https://github.com/ahmetb/kubectx /opt/kubectx && \
+  ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx && \
+  ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+)
 
-echo '[STEP 9] Setting zsh as default shell'
-chsh -s $(which zsh) vagrant
+echo '[COPYING] Copy dotfiles over exisitng ones'
+cp ~/.dotfiles/.zshrc ~/.zshrc
+
+echo '[SETUP] Setting zsh as default shell'
+chsh -s $(which zsh)
+
+echo '[END] Provisioning Process Finished'

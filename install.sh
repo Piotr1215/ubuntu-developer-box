5c746601f5c276568a58315c179e5d8976918e89
decoder <decoder@lilve.de>
2020-11-30 17:26:00 +0100

    vagrant specific user role

--- install.sh
+++ install.sh
@@ -19,17 +19,17 @@ echo '[STEP 4] Installing Oh-My-Zsh'
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo '[STEP 5] Installing zsh-autosuggestions plugin'
-git clone https://github.com/zsh-users/zsh-autosuggestions /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions /home/vagrant/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 echo '[STEP 6] Install tmux with cool customizations'
-git clone https://github.com/samoshkin/tmux-config.git
-./tmux-config/install.sh
+git clone https://github.com/samoshkin/tmux-config.git /home/vagrant/tmux-config
+/home/vagrant/tmux-config/install.sh
 
 echo '[STEP 7] Clone my dotfiles'
-git clone git://github.com/Piotr1215/dotfiles.git ~/.dotfiles
+git clone git://github.com/Piotr1215/dotfiles.git /home/vagrant/.dotfiles
 
 echo '[STEP 8] Copy dotfiles over exisitng ones'
-cp ~/.dotfiles/.zshrc ~/.zshrc
+cp ~/.dotfiles/.zshrc /home/vagrant/.zshrc
 
 echo '[STEP 9] Setting zsh as default shell'
-chsh -s $(which zsh)
+chsh -s $(which zsh) vagrant
